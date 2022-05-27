88dd18c68281adc98a244a7b363bef8fd0923466
Ximena Valenzuela <xduarte@uabc.edu.mx>
2022-05-27 01:24:31 -0700

    Agregar archivos de proyecto.

---
+++ CADENA/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADENA
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            String cadena;
+            int num1 = 5, num2, resultado;
+
+            Console.Write("Dame un numero para sumarlo: ");
+            cadena = Console.ReadLine();
+            num2 = Int32.Parse(cadena);
+            resultado = num1 + num2;
+            Console.WriteLine("El resultado es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CADENA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADENA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            Console.WriteLine("Como te llamas: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("Hola" + " " + nombre + " " + "Quieres ver el menu? ");
+            Console.ReadKey();
+        }
+    }
+}
